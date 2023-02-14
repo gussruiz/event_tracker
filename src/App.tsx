cb5f8e87bc8b403527e17a376d5df6b345e1e0a3
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-14 20:23:47 -0300

    initial commit on branch, fixing dates

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,15 @@ function App() {
   const [eventos, setEventos] = useState<IEvento[]>([
     {
         "descricao": "Estudar React",
-        "inicio": new Date("2022-01-15T09:00"),
-        "fim": new Date("2022-01-15T13:00"),
+        "inicio": new Date("2023-02-15T09:00"),
+        "fim": new Date("2023-02-15T13:00"),
         "completo": false,
         "id": 1642342747
     },
     {
         "descricao": "Estudar Recoil",
-        "inicio": new Date("2022-01-16T09:00"),
-        "fim": new Date("2022-01-16T11:00"),
+        "inicio": new Date("2023-02-16T09:00"),
+        "fim": new Date("2023-02-16T11:00"),
         "completo": false,
         "id": 1642342959
     }
